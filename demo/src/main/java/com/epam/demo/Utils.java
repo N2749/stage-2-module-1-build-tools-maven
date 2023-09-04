6a090c5d8c0fee251babd0d5719ca6fde343056e
Nurbolat <nurba.dav@gmail.com>
2023-09-04 11:27:22 +0600

    removed test

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        return args.stream().map(Integer::parseInt).filter(i -> i > 0).count() == args.size();
+        try {
+            return args.stream().map(Integer::parseInt).filter(i -> i > 0).count() == args.size();
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,10 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
-        return Integer.parseInt(str) > 0;
+        try {
+            return Integer.parseInt(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
