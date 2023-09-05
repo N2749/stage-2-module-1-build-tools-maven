58e393ccac45bb766fa8ee3f336925bf0ecd742e
Nurbolat <nurba.dav@gmail.com>
2023-09-05 16:39:05 +0600

    refactored Utils class to use StringUtils class

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,12 +1,14 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
         try {
-            return args.stream().map(Integer::parseInt).filter(i -> i > 0).count() == args.size();
+            return args.stream().filter(StringUtils::isPositiveNumber).count() == args.size();
         } catch (NumberFormatException e) {
             return false;
         }
